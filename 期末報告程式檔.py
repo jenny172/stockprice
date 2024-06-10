4b1d384ce2af9296894f636858f972c86165d024
jenny172 <167406557+jenny172@users.noreply.github.com>
2024-06-10 13:27:40 +0800

    Add files via upload

--- 期末報告程式檔.py
+++ 期末報告程式檔.py
@@ -13,7 +13,7 @@ os.chdir("D:\金融大數據\期末報告")
 YahooNews=pd.read_csv('航運報導.csv')
 
 ###############################
-###Part I: Identify the Nose###
+###Part I: Identify the Noise###
 ###############################
 import re
 
@@ -31,12 +31,12 @@ YahooNews.columns
 YahooNews[['Context', 'Impurity']].sort_values(by='Impurity', ascending=False).head(6)
 
 #####################################################
-###Part II: Removing Nose with Regular Expressions###
+###Part II: Removing Noise with Regular Expressions###
 #####################################################
 
-#remark: html.unescape
+#remark: html.unescape()，converts HTML-safe sequences (&lt;, &gt;, etc.) into their corresponding characters (<, >, etc.)
 import html
-p = '&lt;abc&gt;' #&lt; and &gt; are special simbles in html
+p = '&lt;abc&gt;' #&lt; and &gt; are special symbols in html
 #not showing in text example
 txt= html.unescape(p)
 print (txt)
@@ -69,8 +69,7 @@ YahooNews['Clean_text'] = YahooNews['Context'].apply(clean)
 #加入不純度(Impurity)變數
 YahooNews['Impurity']   = YahooNews['Clean_text'].apply(impurity, min_len=20)
 #顯示清理後的前六個文本之不純度
-YahooNews[['Clean_text', 'Impurity']].sort_values(by='Impurity', ascending=False) \
-                              .head(6)     
+YahooNews[['Clean_text', 'Impurity']].sort_values(by='Impurity', ascending=False).head(6)     
 
 
 ####################################################
@@ -115,7 +114,7 @@ def remove_stop(text):
     #filtered_words = [w for w in words if w not in stopwords1 and w.strip() != '']
     #return list(filtered_words)
 
-#分詞並移除停用詞
+#斷詞並移除停用詞
 YahooNews['tokens']=YahooNews['Context'].apply(jieba.cut)
 YahooNews['tokens_new']=YahooNews['tokens'].apply(remove_stop)
 YahooNews.iloc[0,:]
@@ -153,15 +152,14 @@ plt.imshow(wordcloud)
 
 
 
-
-#將分詞結果轉完字串
+#將斷詞結果轉為字串
 def list_to_string(org_list, seperator=' '):
     return seperator.join(org_list)
 
 YahooNews['News_seg']=YahooNews['tokens_new'].apply(list_to_string)
 YahooNews['News_seg'][1]
 
-
+#將YahooNews['News_seg']轉為文章-字詞矩陣
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(decode_error='ignore', min_df=2) 
 
@@ -169,13 +167,16 @@ dt01 = cv.fit_transform(YahooNews['News_seg'])
 print(cv.get_feature_names_out())
 fn=cv.get_feature_names_out()
 
-
+# Convert the sparse matrix to a dense array and create a DataFrame
 dtmatrix=pd.DataFrame(dt01.toarray(), columns=cv.get_feature_names_out())
+print(dtmatrix)
 
 
+#cosine_similarity是計算各向量間每行的相似度
 from sklearn.metrics.pairwise import cosine_similarity
 cosine_similarity(dt01[20], dt01[24])
 
+#計算文章間的相似性
 sm = pd.DataFrame(cosine_similarity(dt01, dt01))
 
 
@@ -187,16 +188,18 @@ YahooNews['Context'][24]
 from sklearn.feature_extraction.text import TfidfTransformer
 tfidf = TfidfTransformer()
 
-
 tfidf_dt = tfidf.fit_transform(dt01)
 tfidfmatrix = pd.DataFrame(tfidf_dt.toarray(), columns=cv.get_feature_names_out())
+
+#cosine_similarity是計算各向量間每行的相似度
 cosine_similarity(tfidf_dt[20], tfidf_dt[22])
 
-
+#計算各字詞間的相似性
 sm1 =pd.DataFrame(cosine_similarity(tfidf_dt, tfidf_dt))
 
-
+#計算經過TF-IDF轉換後文章間的相似性
 sm2 = pd.DataFrame(cosine_similarity(tfidf_dt.transpose(), tfidf_dt.transpose()))
+
 
 YahooNews['Context'][20]
 YahooNews['Context'][22]
@@ -213,6 +216,8 @@ wordcloud.generate_from_frequencies(tfidfsum)
 #plt.figure(figsize=(20,10)) 
 plt.imshow(wordcloud)
 
+
+#集群分析
 from sklearn.cluster import KMeans
 
 from sklearn import preprocessing 
@@ -233,7 +238,7 @@ plt.xlabel('Number of clusters')
 plt.ylabel('Distortion')
 plt.show()
 
-
+#分成五群
 km = KMeans(
     n_clusters=5, init='random',
     n_init=10, max_iter=300, 
@@ -241,16 +246,40 @@ km = KMeans(
 )
 y_km = km.fit_predict(preprocessing.normalize(tfidf_dt))
 
-g0 = YahooNews['Context'][y_km==0]
-g0.head()
-g1 = YahooNews['Context'][y_km==1]
-g1.head()
-g2 = YahooNews['Context'][y_km==2]
-g2.head()
-g3 = YahooNews['Context'][y_km==3]
-g3.head()
-g4 = YahooNews['Context'][y_km==4]
-g4.head()
+
+# 將聚集類別標籤加入到DataFrame中
+YahooNews['Cluster'] = y_km
+
+
+
+###Creating Word Clouds
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud ###
+from collections import Counter ###
+
+# 繪製各分群之文字雲
+for cluster in range(5):
+    cluster_data = YahooNews[YahooNews['Cluster'] == cluster]
+    text = ' '.join(cluster_data['News_seg'])
+    wordcloud = WordCloud(font_path="SimHei.ttf", background_color="white").generate(text)
+    
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.title(f'Cluster {cluster}')
+    plt.show()
+
+
+#g0 = YahooNews['Context'][y_km==0]
+#g0.head()
+#g1 = YahooNews['Context'][y_km==1]
+#g1.head()
+#g2 = YahooNews['Context'][y_km==2]
+#g2.head()
+#g3 = YahooNews['Context'][y_km==3]
+#g3.head()
+#g4 = YahooNews['Context'][y_km==4]
+#g4.head()
 
 
 
@@ -287,8 +316,8 @@ YNews.groupby('day_of_week').size().plot.bar()
 #計算文章來自各網站的次數
 YahooNews['From'].value_counts()
 
-#尋找各文章和標題包含關鍵字的字數
-YahooNews[YahooNews['Context'].str.contains('貨櫃')].From.count()
+#尋找各文章和標題包含關鍵字的次數
+#YahooNews[YahooNews['Context'].str.contains('貨櫃')].From.count()   #計算總次數
 YahooNews[YahooNews['Context'].str.contains('貨櫃')].From.value_counts()
 YahooNews[YahooNews['Title'].str.contains('貨櫃')].From.value_counts()
 
